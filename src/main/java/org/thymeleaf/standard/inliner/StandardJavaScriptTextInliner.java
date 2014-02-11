41994b542371c84eed52e07f5d0c80b4419a2ed6
Hsiu-Yuan Shan <hyshan@cht.com.tw>
2014-02-11 13:38:07 +0800

    Change version name and use Jackson for json deserialize.

--- src/main/java/org/thymeleaf/standard/inliner/StandardJavaScriptTextInliner.java
+++ src/main/java/org/thymeleaf/standard/inliner/StandardJavaScriptTextInliner.java
@@ -1,49 +1,55 @@
 /*
  * =============================================================================
- * 
+ *
  *   Copyright (c) 2011-2013, The THYMELEAF team (http://www.thymeleaf.org)
- * 
+ *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
  *   You may obtain a copy of the License at
- * 
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *   Unless required by applicable law or agreed to in writing, software
  *   distributed under the License is distributed on an "AS IS" BASIS,
  *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
- * 
+ *
  * =============================================================================
  */
 package org.thymeleaf.standard.inliner;
 
-import org.thymeleaf.util.JavaScriptUtils;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
- * 
+ *
  * @author Daniel Fern&aacute;ndez
- * 
+ * @author hyshan
  * @since 1.1.2
  *
  */
 public class StandardJavaScriptTextInliner extends AbstractStandardScriptingTextInliner {
-    
-    
+
+
     public static final StandardJavaScriptTextInliner INSTANCE = new StandardJavaScriptTextInliner();
-    
-    
+
+
     private StandardJavaScriptTextInliner() {
         super();
     }
 
-    
-    
-    
+
+
+
     @Override
     protected String formatEvaluationResult(final Object result) {
-        return JavaScriptUtils.print(result);
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writeValueAsString(result);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
